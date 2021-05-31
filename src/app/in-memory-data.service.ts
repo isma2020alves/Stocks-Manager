16f09f6bf5c0df97da2a2ada9a9dc9b6790609a4
isma2020alves <73471932+isma2020alves@users.noreply.github.com>
2021-05-31 21:31:43 +0100

    Http Get request set for getStocks instead of using the mock API.

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Stock } from './stock';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const stocks = [
+      { id: 1, name: 'Apple'},
+      { id: 2, name: 'Tesla'},
+      { id: 3, name: 'Microsoft'},
+      { id: 4, name: 'Jp Morgan'},
+      { id: 5, name: 'Google'},
+      { id: 6, name: 'Amazon'},
+      { id: 7, name: 'Alibaba'}
+    ];
+    return {stocks};
+  }
+ // Overrides the genId method to ensure that a hero always has an id.
+  // If the heroes array is empty,
+  // the method below returns the initial number (1).
+  // if the heroes array is not empty, the method below returns the highest
+  // hero id + 1.
+  genId(stocks: Stock[]): number {
+    return stocks.length > 0 ? Math.max(...stocks.map(stock => stock.id)) + 1 : 1;
+  }
+}
--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -3,16 +3,21 @@ import { Stock } from './stock';
 import { STOCKS } from './mock-stocks';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class StockService {
-
+  
+  private stocksUrl = 'api/stocks'; // URL to web api
+  
+/** GET heroes from the server */
 getStocks(): Observable<Stock[]> {
-  const stocks  = of(STOCKS);
-  this.messageService.add('StockService: fetched stocks');
-  return stocks;
+  // const stocks  = of(STOCKS);
+  // this.messageService.add('StockService: fetched stocks');
+  return this.http.get<Stock[]>(this.stocksUrl);
 }
 
 getStock(id: number): Observable<Stock> { 
@@ -22,5 +27,12 @@ getStock(id: number): Observable<Stock> {
   this.messageService.add(`StockService: fetched stock id=${id}`);
   return of(stock);
 }
-  constructor(private messageService: MessageService) { }
+constructor (
+  private http: HttpClient,
+  private messageService: MessageService) { }
+  
+private log(message: string) {
+  this.messageService.add(`HeroService: ${message}`);
+  }
 }
+    
