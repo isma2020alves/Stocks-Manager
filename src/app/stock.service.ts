43b535a5dabb18db4cefdfdd4427bb97dd02a03d
isma2020alves <73471932+isma2020alves@users.noreply.github.com>
2021-06-01 16:22:56 +0100

    Added http request getStock by Id.

--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -4,6 +4,7 @@ import { STOCKS } from './mock-stocks';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import  { catchError, map, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -17,15 +18,20 @@ export class StockService {
 getStocks(): Observable<Stock[]> {
   // const stocks  = of(STOCKS);
   // this.messageService.add('StockService: fetched stocks');
-  return this.http.get<Stock[]>(this.stocksUrl);
+  return this.http.get<Stock[]>(this.stocksUrl)
+  .pipe(
+    tap(_ => this.log('fetched  stocks')),
+    catchError(this.handleError<Stock[]>('getStocks', []))
+  );
 }
 
+/** GET stock by id. Will 404 if id not found */
 getStock(id: number): Observable<Stock> { 
-  // For now, assume that a hero with the specified `id` always exists.
-  // Error handling will be added in the next after.
-  const stock = STOCKS.find(s => s.id === id)!;
-  this.messageService.add(`StockService: fetched stock id=${id}`);
-  return of(stock);
+  const url = `${this.stocksUrl}/${id}`;
+  return this.http.get<Stock>(url).pipe(
+    tap(_ => this.log('fetched stock id=${id}')),
+    catchError(this.handleError<Stock>('getStock id=${id}'))
+  )
 }
 constructor (
   private http: HttpClient,
@@ -34,5 +40,25 @@ constructor (
 private log(message: string) {
   this.messageService.add(`HeroService: ${message}`);
   }
+
+/**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+
+private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 }
     
