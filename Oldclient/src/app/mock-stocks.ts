5ca78f4196c3ecf5cb4dd7131b4f2512aa7428db
isma2020alves <73471932+isma2020alves@users.noreply.github.com>
2021-12-19 13:46:29 +0000

    Removed Angular Client from solution

--- Oldclient/src/app/mock-stocks.ts
+++
@@ -1,6 +0,0 @@
-import { IStock } from './stock';
-
-export const STOCKS: IStock[] = [
-    {id: 1, name: 'Apple', ticker: 'AAPL', value: 100},
-    {id: 2, name: 'Tesla', ticker: 'TSLA', value: 500}
-];
--- Oldclient/src/app/stock.ts
+++
@@ -1,6 +0,0 @@
-export interface IStock {
-    id: number;
-    name: string;
-    value: number;
-    ticker: string;
-}
