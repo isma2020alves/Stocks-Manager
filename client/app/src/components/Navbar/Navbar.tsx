90911805b42522c7935b1e52f0e0ff384d3444f3
isma2020alves <73471932+isma2020alves@users.noreply.github.com>
2021-12-22 14:04:09 +0000

    Started_feat_Navbar

---
+++ client/app/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <body>
+      <ul>
+        <li><a>Stocks Manager</a></li>
+      </ul>
+    </body>
+  );
+};
+
+export default Navbar;
---
+++ client/app/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <body>
+            Stonks!
+        </body>
+    );
+};
+
+export default Home;
